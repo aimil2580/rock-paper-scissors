c155647144799820aafdccdbe95550348034a4f5
aimil2580 <milidakisaimilios@gmail.com>
2024-06-29 16:15:36 +0300

    pc and human logic build, working on main game

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 console.log('hello world');
+let humanScore = 0;
+let comptuerScore = 0;
+
+//function for pc logic
+function getComputerChoice(parm1){
+    if (parm1 >= 0 && parm1 < 0.33){
+        return "rock"
+    }else if (parm1 >= 0.33 && parm1 < 0.66){
+           return "paper"
+    }else{
+        return "scissors"
+    }
+}
+// console.log(getComputerChoice(Math.random()))
+
+//fuction for human logic
+function getHumanChoice(parm1){
+    let correctParam = parm1.toLowerCase()
+    return correctParam
+}
+// console.log(getHumanChoice(prompt("rock paper or scissors?")));
