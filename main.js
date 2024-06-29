e3aae6beb4c0ec4662069b891271d655db92eed6
aimil2580 <milidakisaimilios@gmail.com>
2024-06-29 20:07:53 +0300

    done but no ui

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ let humanScore = 0;
 let comptuerScore = 0;
 
 //function for pc logic
-function getComputerChoice(parm1){
+
+function getComputerChoice(){
+    let parm1 = Math.random();
     if (parm1 >= 0 && parm1 < 0.33){
         return "rock"
     }else if (parm1 >= 0.33 && parm1 < 0.66){
@@ -11,14 +13,22 @@ function getComputerChoice(parm1){
         return "scissors"
     }
 }
-// console.log(getComputerChoice(Math.random()))
+
 
 //fuction for human logic
 function getHumanChoice(parm1){
-    let correctParam = parm1.toLowerCase()
-    return correctParam
+    let correctParam = parm1.toLowerCase();
+    if (correctParam == "r"){
+        return "rock";
+    }
+    if (correctParam == "p"){
+        return "paper";
+    }
+    if (comptuerScore == "s"){
+        return "scissors";
+    }
+    return correctParam;
 }
-// console.log(getHumanChoice(prompt("rock paper or scissors?")));
 
 //one-round game logic
 function playRound(humanChoice,ComputerChoice){
@@ -28,24 +38,30 @@ function playRound(humanChoice,ComputerChoice){
                 case "rock":
                     return "it's a tie! no one get points";
                 case "paper":
+                    comptuerScore++;
                     return "You lose! Paper beats rock :("
                 case "scissors":
+                    humanScore++;
                     return "You win! Rock beats scissors ;)";
             }
         case "paper":
             switch (ComputerChoice) {
                 case "rock":
+                    humanScore++;
                     return "You win! Paper beats rock ;)";
                 case "paper":
                     return "it's a tie! no one get points"
                 case "scissors":
+                    comptuerScore++;
                     return "You lose! Scissors beats paper :(";
             }
         case "scissors":
             switch (ComputerChoice) {
                 case "rock":
+                    comptuerScore++;
                     return "You lose! Rock beats scissors :(";
                 case "paper":
+                    humanScore++;
                     return "You win! Scissors beats paper ;)"
                 case "scissors":
                     return "it's a tie! no one get points";
@@ -53,4 +69,17 @@ function playRound(humanChoice,ComputerChoice){
         }
     }
 
-console.log(playRound(getHumanChoice(prompt("rock paper or scissors")),getComputerChoice(Math.random())));
+
+function playGame(){
+    while (humanScore !=5 && comptuerScore !=5 ){
+        console.log(playRound(getHumanChoice(prompt("rock paper or scissors")),getComputerChoice()));
+        console.log("You:",humanScore,"Computer:",comptuerScore)
+    }
+} 
+
+console.log(playGame());
+if (humanScore > comptuerScore){
+    console.log("You won!");
+}else{
+    console.log("You lost. :(")
+}
