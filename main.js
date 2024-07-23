3f78ea8c4e7d07437877a34f58dcfb000834faca
aimil2580 <milidakisaimilios@gmail.com>
2024-07-24 02:21:46 +0300

    done ui+replay btn

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 let humanScore = 0;
 let comptuerScore = 0;
-let score = document.querySelector(".score");
-let messageBox = document.querySelector(".winLoseMes")
+const score = document.querySelector(".score");
+const messageBox = document.querySelector(".winLoseMes")
 const win = "Game over, YOU WIN!";
 const lose = "Game over, YOU LOSE :((";
+const playerMove = document.querySelector(".pcol")
+const computerMove = document.querySelector(".ccol")
+
 // let humanChoice="";
 
 
@@ -28,9 +31,20 @@ function getHumanChoice() {
     let playerRock = document.querySelector("#rock")
     playerRock.addEventListener("click", () => {
         if (humanScore != 5 && comptuerScore != 5) {
-            console.log(humanScore, comptuerScore)
+            //human display selection
+            const div = document.createElement("div");
+            div.classList.add("moves")
+            div.textContent = "Rock";
+            playerMove.appendChild(div)
+            //display selection for pc
+            const divPc = document.createElement("div")
+            divPc.classList.add("moves")
+
             switch (getComputerChoice()) {
                 case "rock":
+
+                    divPc.textContent = "Rock"
+                    computerMove.appendChild(divPc)
                     messageBox.textContent = "it's a tie! no one gets points";
                     score.textContent = `${humanScore} - ${comptuerScore}`;
                     break;
@@ -41,6 +55,8 @@ function getHumanChoice() {
                     } else {
                         messageBox.textContent = "You lose! Paper beats rock :(";
                     }
+                    divPc.textContent = "Paper"
+                    computerMove.appendChild(divPc)
                     score.textContent = `${humanScore} - ${comptuerScore}`;
                     break;
                 case "scissors":
@@ -50,6 +66,8 @@ function getHumanChoice() {
                     } else {
                         messageBox.textContent = "You win! Rock beats scissors ;)";
                     }
+                    divPc.textContent = "Scissors"
+                    computerMove.appendChild(divPc)
                     score.textContent = `${humanScore} - ${comptuerScore}`;
                     break;
             }
@@ -59,28 +77,42 @@ function getHumanChoice() {
     let playerPaper = document.querySelector("#paper")
     playerPaper.addEventListener("click", () => {
         if (humanScore != 5 && comptuerScore != 5) {
-            console.log(humanScore, comptuerScore)
+            //human display selection
+            const div = document.createElement("div");
+            div.classList.add("moves")
+            div.textContent = "Paper";
+            playerMove.appendChild(div)
+
+            const divPc = document.createElement("div")
+            divPc.classList.add("moves")
+
             switch (getComputerChoice()) {
                 case "rock":
                     humanScore++;
-                    if (humanScore == 5){
+                    if (humanScore == 5) {
                         messageBox.textContent = win
                     } else {
                         messageBox.textContent = "You win! Paper beats rock ;)";
                     }
+                    divPc.textContent = "Rock"
+                    computerMove.appendChild(divPc)
                     score.textContent = `${humanScore} - ${comptuerScore}`;
                     break;
                 case "paper":
+                    divPc.textContent = "Paper"
+                    computerMove.appendChild(divPc)
                     messageBox.textContent = "it's a tie! no one gets points"
                     score.textContent = `${humanScore} - ${comptuerScore}`;
                     break;
                 case "scissors":
                     comptuerScore++;
-                    if (comptuerScore == 5){
+                    if (comptuerScore == 5) {
                         messageBox.textContent = lose
                     } else {
                         messageBox.textContent = "You lose! Scissors beats paper :(";
                     }
+                    divPc.textContent = "Scissors"
+                    computerMove.appendChild(divPc)
                     score.textContent = `${humanScore} - ${comptuerScore}`;
                     break;
             }
@@ -90,26 +122,41 @@ function getHumanChoice() {
     let playerScissors = document.querySelector("#scissors")
     playerScissors.addEventListener("click", () => {
         if (humanScore != 5 && comptuerScore != 5) {
-            console.log(humanScore, comptuerScore)
+            //human display selection
+            const div = document.createElement("div");
+            div.classList.add("moves")
+            div.textContent = "Scissors";
+            playerMove.appendChild(div)
+
+            const divPc = document.createElement("div")
+            divPc.classList.add("moves")
+
             switch (getComputerChoice()) {
                 case "rock":
                     comptuerScore++;
-                    if (comptuerScore == 5){
+                    if (comptuerScore == 5) {
                         messageBox.textContent = lose
+                    } else {
+                        messageBox.textContent = "You lose! Rock beats scissors :(";
                     }
-                    messageBox.textContent = "You lose! Rock beats scissors :(";
+                    divPc.textContent = "Rock"
+                    computerMove.appendChild(divPc)
                     score.textContent = `${humanScore} - ${comptuerScore}`;
                     break;
                 case "paper":
                     humanScore++;
-                    if (humanScore == 5){
+                    if (humanScore == 5) {
                         messageBox.textContent = win
                     } else {
                         messageBox.textContent = "You win! Scissors beats paper ;)"
                     }
+                    divPc.textContent = "Paper"
+                    computerMove.appendChild(divPc)
                     score.textContent = `${humanScore} - ${comptuerScore}`;
                     break;
                 case "scissors":
+                    divPc.textContent = "Scissors"
+                    computerMove.appendChild(divPc)
                     messageBox.textContent = "it's a tie! no one gets points";
                     score.textContent = `${humanScore} - ${comptuerScore}`;
                     break;
@@ -117,9 +164,4 @@ function getHumanChoice() {
         }
     })
 }
-
-
-
 console.log(getHumanChoice())
-console.log(humanScore, comptuerScore)
-
